0e57404a41a633d6427395873a7313baa6911706
LReyna <leo.reyna@gmail.com>
2023-01-22 10:12:01 -0500

    added comments

--- fylo2.py
+++ fylo2.py
@@ -1,22 +1,30 @@
+# RL Jan 2023
+# Example of using termcolor, if statements, accessing a text file to 
+# read and write, special characters, etc. 
+
+
 import os
 while True:
     os.system("cls")
+    from termcolor import colored, cprint
 
-    print("Sample File Reading and Writing")
-    print("-------------------------------")
+    cprint("-------------------------------", "magenta")
+    cprint("Sample File Reading and Writing", "magenta")
+    cprint("-------------------------------", "magenta")
 
-    question = "Would you like to add a line of text to the sample file (\"a\" to ADD, \"r\" to READ the file. \"e\" to EXIT)? "
+    question = "Would you like to do? "
     message = f"Hello! {question}"
-    print(message)
+    cprint(message, "blue")
 
-    seleccion = input("Enter \"a\" or \"r\" or Type \"e\" to Exit ") 
+    seleccion = input("Enter \"a\" to add test • \"r\" to read file • \"c\" to clear the file • \"e\" to Exit ") 
 
     if seleccion == "a":
         newline = input("Enter the word you would like to add: ")
         newlinewithbrk = f"\n {newline}"
         with open("/python-othercode/fylo/samplefile.txt", "a") as file:
             file.write(newlinewithbrk)
-            print("Added text!")
+            cprint("Added text!", "magenta")
+            input("Press any key to continue")           
 
     elif seleccion == "r":
         print("Reading the file...")
@@ -27,7 +35,18 @@ while True:
     elif seleccion == "e":
         print("Exiting now..")
         break
+
+    elif seleccion =="c":
+        clearselection = input("Are you sure you want to clear the contents of the file (cannot be undone) y/n ")
+        if clearselection == "y":
+            with open("/python-othercode/fylo/samplefile.txt", "w") as file:
+                file.write("")
+                input("Press any key to continue...")
+                print("File contents were cleared!")
+                 
+        elif clearselection == "n":
+            print("Exiting now..")
         
     else:
-        print("Invalid input. Please enter y or n. or e to exit")
+        print("Invalid input. Enter \"a\" to add a line • \"r\" to read file • \"c\" to clear the file • \"e\" to Exit")
         input("Press enter to continue...")
