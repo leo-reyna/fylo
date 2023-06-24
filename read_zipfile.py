c6d5312e08c065ac80ff8a29763434a0b3301568
LReyna <leo.reyna@gmail.com>
2023-06-23 21:15:19 -0400

    commit2

--- read_zipfile.py
+++ read_zipfile.py
@@ -8,10 +8,10 @@ def ReadZipFile(file_path):
             print(file_info.filename)
         
         # Extract and print the contents of a specific file
-        specific_file = input("Enter the name of a file to read its contents: ")
-        if specific_file in ZipReferencia.namelist():
-            with ZipReferencia.open(specific_file) as file:
-                print(f"\nShowing contents of '{specific_file}':")
+        ArchivoEspecifico = input("Enter the name of a file to read its contents: ")
+        if ArchivoEspecifico in ZipReferencia.namelist():
+            with ZipReferencia.open(ArchivoEspecifico) as file:
+                print(f"\nShowing contents of '{ArchivoEspecifico}':")
                 print(file.read().decode())
         else:
             print("File not found in the zip file.")
